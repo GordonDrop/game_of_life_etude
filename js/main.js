dd460b19a35c3a503026c95e422b6588d7912f6c
const <ksimanov@gmail.com>
2014-04-04 02:28:40 +0700

    Remove console.logs, Change algorithm to check cell status

--- js/main.js
+++ js/main.js
@@ -1,200 +1,194 @@
-var genCnt = 1;
-
-// TODO: loop stuck at second genera
-function main() 
+(function main()
 {
-	// constants
-	var width = 25;
-	var height = 15;
-	var timeout = 1000;
-	var domElem = $( '#world' );
-	var clearElem = 'tr';
-	var x = 0;
-	var saveWorldState; 
+    // strange behavior detected
+    // cell = Math.floor(Math.random() * 2);
+    // console.log( ._isNumber( cell ); )
+    // true
+    // worldState[i][j].push( cell );
+    // console.log( ._isArray( cell );) 
+    // true
 
-					test=   [[0,0,0],[1,1,1],[0,0,0]];   
-						 //   [[1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,1,0,0,0,0,0,0],
-							// [0,1,0,1,0,0,1,0,0,0,0,0,0,0,1,0,0,1,0,1,0,0,0,0,0],
-							// [0,0,1,1,1,1,0,1,0,0,0,1,0,1,0,1,1,0,1,0,0,0,0,0,0],
-							// [0,0,0,1,1,1,1,0,1,1,0,0,1,0,1,0,1,1,0,0,0,0,0,0,0],
-							// [0,0,0,0,1,1,0,1,0,1,1,0,0,0,1,1,1,0,0,0,0,1,0,0,0],
-							// [0,1,0,0,0,1,0,0,1,1,1,1,0,1,0,1,1,1,0,0,0,0,1,0,0],
-							// [1,0,1,0,0,1,0,0,0,1,1,0,0,0,1,1,0,0,0,0,0,1,1,0,0],
-							// [0,1,0,0,1,1,1,0,0,0,0,0,0,0,1,0,1,0,0,0,1,0,1,1,0],
-							// [0,1,0,0,0,1,1,0,0,0,1,1,0,0,0,1,0,0,0,0,0,1,0,1,1],
-							// [0,0,1,0,1,0,0,1,0,0,0,1,1,0,1,0,0,0,0,0,0,1,1,0,0],
-							// [1,0,1,1,0,1,1,1,1,1,0,0,0,0,0,1,0,0,1,1,0,0,1,1,0],
-							// [0,1,0,1,1,0,1,1,1,1,1,1,0,0,0,0,1,0,0,1,1,0,1,0,1],
-							// [0,0,0,0,1,1,0,1,0,1,1,0,1,1,0,0,0,0,0,0,1,0,0,1,0],
-							// [0,0,0,0,0,1,1,1,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0],
-							// [0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0]];
-	console.log( 'die')
 
-	var model;
-	var view;
+    // TODO:
+    //  1. Create infinte wrapper
+    //  2. Split Update View in actions
+    //  3. Add controls to randomizer and examples
+    //  4. create RLE converter
+    //  draft : function updateView( worldState, firstGen ) {}
+    var worldState = gliderGun,
+        domElem = $( '#world' ),
+        clearElem = 'tr',
+        height = 100,
+        width = 100,
+        timeout = 1,
+        model = [],
+        view = '';
 
-	function loop()
-	{	
-		if( !_.isArray( saveWorldState )) {
-			console.log( 'true' );
-			//model = firstGeneration( width, height );
-			model = test;
-		} else {
-			console.log( 'false' );
-			model = updateData( saveWorldState );
-		};
+    function loop()
+    {    
+        if( !_.isArray( worldState )) {
+            model = firstGeneration( width, height );
+        } else {
+            model = updateData( worldState );
+        };
 
-		view = updateView( model );
-		draw( domElem, clearElem, view );
-		saveWorldState = model;
+        view = updateView( model );
+        draw( domElem, clearElem, view );
+        worldState = model;
+        view = null;
+        model = null;
+    };
 
-	};
-	setInterval( loop, timeout );
+    setInterval( loop, timeout );
+})();
 
-	// loop();
-	// loop();
-};
-
-// input OBJECT, STRING, STRING => WORLD
+// return somtg?
+// input DOM OBJECT, STRING, STRING => WORLD
 // domTarget to animate one generation state
 // removeHtml elements to clear from DOM
 // drawHtml html to darw generation
 function draw( domTarget, removeHtml, drawHtml )
 {
-	// clear screen
-	if( domTarget.has( removeHtml )) 
-		{ domTarget.find( removeHtml ).remove(); }
+    // clear screen
+    if( domTarget.has( removeHtml )) 
+        { domTarget.find( removeHtml ).remove(); }
 
-	// draw new generation
-	domTarget.find( 'tbody' ).append( drawHtml );
+    // draw new generation
+    domTarget.find( 'tbody' ).append( drawHtml );
 }
 
-// number, number => array
+// NUMBER, NUMBER => ARRAY
 // input: two numbers
-// output: array which represent world
 // y - size of the first array
 // represents HEIGHT
 // x - size of the nested array
 // represents WIDTH
-function firstGeneration( x, y )
+// output: array which represent first generation
+function firstGeneration ( width, height )
 {
-	console.log( 'create model', genCnt);
-	var cell;
-	var worldState = [];
+    var worldState = [],
+    cell;
 
-	_(y).times(function( i ) 
-	{
-		worldState[i] = [];
-		_(x).times(function( j ) 
-		{
-			cell = Math.floor(Math.random() * 2);
-			worldState[i][j] = [];
-			worldState[i][j].push( cell );		
-		});
-	});
-	return worldState;
+    _( height ).times(function( i ) 
+    {
+        worldState[i] = [];
+        _( width ).times(function( j ) 
+        {
+            cell = Math.floor(Math.random() * 2);
+            worldState[i].push( cell );
+        });
+    });
+    return worldState;
 };
 
+// ARRAY => Array
+// ipnut: current world state
+// output: next generation world state
 function updateData( worldState )
 {
-	console.log( 'update model', genCnt );
-	genCnt++;
-	var yLen = worldState.length;
-	var xLen = worldState[0].length;
-	var newWorldState = _.clone( worldState );
+    var width = worldState[0].length,
+    height = worldState.length,
+    newWorldState = [];
 
-	console.log( '\n' );
-	_(yLen).times(function( y )
-	{	
-		newWorldState[y] = [];
-		_(xLen).times(function( x )
-		{	
-			newWorldState[y][x] = []
-			neighbours = checkNeighbour( y, x, worldState );
-			if ( neighbours === 3 ) console.log( '3' );
+    _(height).times(function( y )
+    {    
+        newWorldState[y] = [];
+        _(width).times(function( x )
+        {    
+            newWorldState[y][x] = [];
+            neighbours = checkNeighbour( y, x, worldState );
+            cellStatus = checkCellStatus( worldState[y][x], neighbours );
 
-			if( worldState[y][x] === 1 ) 
-			{	
-				if ( neighbours < 2 || neighbours > 3 ) 
-				{
-					newWorldState[y][x] = 0;
-				} else {
-					newWorldState[y][x]= 1;
-				}
-			}
-			else
-			{
-				if ( neighbours === 3 ) 
-				{
-					newWorldState[y][x] = 1;
-				} else {
-					newWorldState[y][x] = 0;
-				}
-			}
-		});		
-	});
+            if ( worldState[y][x] === 1 )
+            {
+            	newWorldState[y][x] = ( cellStatus ) ? 1 : 0;  	
+            }
+            else
+            {
+            	newWorldState[y][x] = ( cellStatus ) ? 1 : 0;
+            }
+        });        
+    });
 
-	_.each( newWorldState, function( element, index ) {
-		console.log(
-	    'Element: ' + element + ', ' +
-	    'Index: ' + index )
-	});
-	console.log( '\n' );
-	return newWorldState;
+    return newWorldState;
 };
 
+// NUMBER, NUMBER, ARRAY => NUNBER
+// input: array keys and array 
+// to check Neighbour cells
+// output: Neighbour cells amount
 function checkNeighbour( keyY, keyX, worldState ) 
 {
-	var cnt = 0;
-	var dy = dx = 0;
-	var diffY = diffX = -1;
-	var yLen = worldState.length;
-	var xLen = worldState[0].length;
+    var diffY = diffX = -1,
+    dy = dx = 0,
+    cnt = 0;
 
-	for( var diffY = -1; diffY < 2; diffY += 1 ) 
-	{
-		// check if key out of the array range
-		dy = keyY + diffY;
-		if ( _.isUndefined(worldState[dy]) ) continue;
+    for( var diffY = -1; diffY < 2; diffY += 1 ) 
+    {
+        // check if key out of the array range
+        dy = keyY + diffY;
+        if ( _.isUndefined( worldState[dy] )) continue;
 
-		for( var diffX = -1; diffX < 2; diffX += 1 ) 
-		{
-			dx = keyX + diffX;
+        for( var diffX = -1; diffX < 2; diffX += 1 ) 
+        {
+            dx = keyX + diffX;
 
-			// check if key out of the array range,
-			// dead or it has keY, keyX
-			if ( _.isUndefined(worldState[dy][dx]) || (diffX == 0 && diffY == 0 )) continue;
-			if ( !( worldState[dy][dx] == 0 )) cnt++;
-		}
-	}
-	console.log( cnt )
-	return cnt;
+            // check if key out of the array range,
+            // dead or it has keY, keyX
+            if ( _.isUndefined( worldState[dy][dx] ) || ( diffX === 0 && diffY === 0 )) continue;
+            if ( worldState[dy][dx] === 1 ) cnt += 1;
+        }
+    }
+    return cnt;
 }
 
+// NUMBER => BOOLEAN
+// input: neighnours amount
+// output: next generation cell status
+function checkCellStatus( cell, neighbours )
+{
+	if ( neighbours < 2 ) {
+		return false;
+	} 
+	else if ( neighbours > 3 ) 
+	{
+		return false;
+	} 
+	else if ( neighbours === 2 )
+	{
+		return cell;
+	} 
+	else if ( neighbours === 3 )
+	{
+		return true;
+	}
+}
+
+// ARRAY => STRING
+// input: ARRAY coverted to string
+// output: STRING to append in html
 function updateView( worldState ) 
-{	
-	var alive = '<td class="alive"></td>';
-	var dead = '<td></td>';
-	var close = '</tr>';
-	var open = '<tr>';
-	var html = '';
+{    
+    var alive = '<td class="alive"></td>',
+    dead = '<td></td>',
+    close = '</tr>',
+    open = '<tr>',
+    html = '';
 
-	_.each( worldState, function( element ) 
-	{ 
-	   	html += open;
+    _.each( worldState, function( element ) 
+    { 
+        html += open;
 
-	    _.each( element, function( subEl ) 
-	    {
-	        if ( subEl == 1 ) {
-	          html += alive;
-	        } else {
-	          html += dead;	          
-	        };
-	    });
-  		html += close; 
-	});
+        _.each( element, function( subEl ) 
+        {
+            if ( subEl == 1 ) {
+                html += alive;
+            } else {
+                html += dead;              
+            };
+        });
+        html += close; 
+    });
 
-	return html;
+    return html;
 };
-
-main();
