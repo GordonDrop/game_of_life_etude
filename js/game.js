79dd5f885fad721fa8bcbafaee9ce261c52dfb54
randy <ksimanov@gmail.com>
2015-02-22 01:46:43 +0700

    Refuck logic

---
+++ js/game.js
@@ -0,0 +1,107 @@
+// Contents
+// 1. Grid
+// 2. Display
+
+// TODO:
+// 1. refuck to proto
+// 2. add options to change speed of animation, menu of examples and options to pause, choose random
+// 3. add canvas
+// 4. function to detect stable generations or dead worlds
+
+function makeGrid( width, height, isRandom ) {
+    var grid = [];
+
+    for ( var y = 0; y < width; y++ ) {
+        var gridLine = [];
+        for( var x = 0; x < height; x++ ) {
+            var cell = isRandom ?  Math.random() < 0.3 : false;
+            gridLine.push( cell ) ;
+        }
+        grid.push( gridLine );
+    }
+    return grid;
+};
+
+function nextGen( grid ) {
+    var newGrid = [];
+
+    for ( var y = 0; y < grid.length; y++ ) {
+        var line = grid[y], gridLine = [];
+        for( var x = 0; x < line.length; x++ ) {
+            var neighborsCnt = countNeighbors( x, y, grid );
+            var cell = isAlive( neighborsCnt, line[x] );
+            gridLine.push( cell );
+        }
+        newGrid.push( gridLine );
+    }
+    return newGrid;
+};
+
+function countNeighbors( x, y, grid ) {
+    var cnt = 0;
+    var dy = y - 1;
+
+    for ( var i = 0; i < 3; i++, dy++ ) {
+        var line = grid[dy], dx = x - 1;
+        if ( !line ) continue;
+        for( var j = 0; j < 3; j++, dx++ )
+            if( ( dx !== x || dy !== y ) && line[dx] ) cnt++;
+    }
+    return cnt;
+}
+
+function isAlive( neighbors, cell ) {
+    var status = '';
+    if ( cell )
+        status = ( neighbors === 2 || neighbors === 3 );
+    else
+        status = ( neighbors === 3 );
+    return status;
+}
+
+function isStable() {
+
+}
+
+// Display
+function element( tagName, className ) {
+    var element = document.createElement( tagName );
+    if ( className ) element.className = className;
+    return element;
+}
+
+function drawFrame( parent, grid ) {
+    var wrap = document.getElementById( parent );
+    var gridTable = element( 'table' );
+    wrap.appendChild( gridTable );
+
+    grid.forEach( function( gridLine ) {
+        var tableRow = element( 'tr' );
+        gridTable.appendChild( tableRow );
+
+        gridLine.forEach( function( cell ) {
+            var className = cell ? 'alive' : '';
+            tableRow.appendChild( element( 'td', className ) );
+        })
+    })
+};
+
+function clearFrame( parent ) {
+    document.getElementById( parent ).innerHTML = '';
+}
+
+function runGame( grid, fps ) {
+    var fps = 15;
+
+    function runAnimation() {
+        setTimeout( function() {
+            clearFrame( 'game' );
+            drawFrame( 'game', grid );
+            grid = nextGen( grid );
+            requestAnimationFrame( runAnimation );
+        }, 1000 / fps );
+    }
+    runAnimation( grid );
+};
+
+runGame( gliderGun );
--- js/helpers.js
+++
@@ -1,5 +0,0 @@
-// Fix javascript Modulo
-// Example: -1 % 3 = 2
-function mod ( m, n ) { 
-    return (( m % n ) + n ) % n; 
-}
